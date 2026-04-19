6bc437dbc825971a7cfa46727940f04ca7c72abe
Muhtasin Jawad <86793961+mutasinjawad@users.noreply.github.com>
2026-04-19 11:59:30 +0600

    ETHSender Form functioning

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -1,16 +1,39 @@
 'use client';
 
 import { useState } from "react";
-
+import { useChainId, useConfig, useAccount } from 'wagmi';
+import { readContract } from "@wagmi/core";
+import { chainsToTSender, erc20Abi, tsenderAbi } from "@/constants";
 import InputField from "./ui/InputField";
 
 export default function AirdropForm() {
     const [recipient, setRecipient] = useState("");
     const [amount, setAmount] = useState("");
     const [tokenAddress, setTokenAddress] = useState("");
+    const chainId = useChainId();
+    const config = useConfig();
+    const account = useAccount();
+
+    async function getApprrovedAmount(tSenderAddress: string | null): Promise<number> {
+        if (!tSenderAddress) {
+            alert("No address found, please use a supported network");
+            return 0;
+        }
+
+        const response = await readContract(config, {
+            abi: erc20Abi,
+            address: tokenAddress as `0x${string}`,
+            functionName: "allowance",
+            args: [account.address, tSenderAddress as `0x${string}`],
+        })
+
+        return response as number;
+    }
 
     async function handleSubmit() {
-        console.log(tokenAddress, recipient, amount);
+        const tSenderAddress = chainsToTSender[chainId]['tsender'];
+        const approvedAmount = await getApprrovedAmount(tSenderAddress);
+        console.log("Approved Amount:", approvedAmount);
     }
 
     return (
