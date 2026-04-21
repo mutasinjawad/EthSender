166dd8a803622b2076df5fd91c95149afa88390c
Muhtasin Jawad <86793961+mutasinjawad@users.noreply.github.com>
2026-04-21 19:34:58 +0600

    Added more network support

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -2,16 +2,18 @@
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
 import { http } from 'wagmi'
-import { anvil, zksync, mainnet } from "wagmi/chains"
+import { anvil, mainnet, sepolia, zksync, zksyncSepoliaTestnet } from "wagmi/chains"
 
 export default getDefaultConfig({
     appName: "TSender",
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    chains: [mainnet, anvil, zksync],
+    chains: [mainnet, anvil, zksync, sepolia, zksyncSepoliaTestnet],
     transports: {
         [mainnet.id]: http('https://cloudflare-eth.com'),
         [zksync.id]: http('https://mainnet.era.zksync.io'),
         [anvil.id]: http('http://127.0.0.1:8545'),
+        [sepolia.id]: http('https://1rpc.io/sepolia'),
+        [zksyncSepoliaTestnet.id]: http('https://rpc.ankr.com/zksync_era_sepolia'),
     },
     ssr: false
 })
